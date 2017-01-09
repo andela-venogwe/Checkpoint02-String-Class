0ade5eb29b08e89a1c7cc7639ff80a0d1169aa5e
andela-venogwe <victor.enogwe@andela.com>
2017-01-09 06:53:55 +0100

    add favicon redirect 404 to home page

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
@@ -12,8 +10,6 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -21,4 +17,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 
+app.use((req, res, next) => {
+  res.status(404);
+  res.redirect('/');
+});
 module.exports = app;
